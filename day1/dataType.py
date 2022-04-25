fc435a7149589a0e47f259f9d4a5683e207e06b0
Priscilla Normando <priscillanormando@gmail.com>
2022-04-25 11:46:47 +0000

    Day 1 and Day 2 update

---
+++ day1/dataType.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+x = None
+y = None
+z = None
+# Read and save an integer, double, and String to your variables.
+x = int(input())
+y = float(input())
+z = str(input())
+# Print the sum of both integer variables on a new line.
+print(i + x)
+# Print the sum of the double variables on a new line.
+print(d + y)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + z)
