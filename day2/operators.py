4cce93211f2132ec2a0bbd3f60c2e79f4da4986e
Priscilla Normando <priscillanormando@gmail.com>
2022-04-29 15:55:22 +0000

    Day2 - solve update

--- day2/operators.py
+++ day2/operators.py
@@ -1,9 +1,5 @@
-meal_cost = float(input())
-tip_percent = int(input())
-tax_percent = int(input())
-
 tip = meal_cost * tip_percent / 100
 tax = meal_cost * tax_percent / 100
-total = meal_cost + tip + tax
-
-print("The total meal cost is " + str(round(total)) + " reais.")
+total_cost = meal_cost + tip + tax
+    
+print(round(total_cost))
